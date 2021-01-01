9447f299371e49d6088fa09cd02568cac455fc2a
Daniel Sanches <danielsanches6301@gmail.com>
2021-01-01 09:40:25 -0300

    A funcao para desenhar o tabuleiro já está funcionando

--- jogoDaVelha.c
+++ jogoDaVelha.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void tabuleiro(casa){
-
+void tabuleiro(char casas[9]){
+   system("cls");
+   printf("\t %c | %c | %c \n",casas[0],casas[1],casas[2]);
+   printf("\t ---------- \n");
+   printf("\t %c | %c | %c \n",casas[3],casas[4],casas[5]);
+   printf("\t ---------- \n");
+   printf("\t %c | %c | %c \n",casas[6],casas[7],casas[8]);
 }
 
 void main() {
-   int casas = {}
+   char casas[9] = {'1','2','3','4','5','6','7','8','9'};
+   tabuleiro(casas);
    system("pause");
 }
