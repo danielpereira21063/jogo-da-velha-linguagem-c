b8eb510d6bbe276558f6b00f88a7aecd666fd4be
Daniel Sanches <danielsanches6301@gmail.com>
2021-01-05 09:42:21 -0300

    Fiz uma refatoracao na chamada das funcoes; Adicionei a funcao jogo que difine o jogador e a dificuldade

--- jogoDaVelha.c
+++ jogoDaVelha.c
@@ -1,15 +1,20 @@
+
+/*
+   Jogo da velha desenvolvido em C
+   by DANIEL PEREIRA SANCHES
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 void main() {
    char casas[9];
    limparTela();
    limparCasas(casas);
-   definirJogador();
-   limparTela();
-   definirDificuldade();
-   limparTela();
+   jogo();
    desenharTabuleiro(casas);
    pausar();
 }
@@ -32,29 +37,33 @@ int definirJogador() {
       printf("\n\t---------------------------------------\n");
       printf("\n\t\t\t1 - amigo \n \t\t\t2 - computador \n");
       printf("\n\t---------------------------------------\n");
-      printf("\n\t\t\tESCOLHA UMA OPCAO: ");
+      printf("\n\t\tESCOLHA UMA OPCAO: ");
       scanf("%d", &jogador);
       if(jogador < 1 || jogador > 2) {
-         system("cls");
+         limparTela();
          printf("\n\t\tOPCAO INVALIDA. TENTE NOVAMENTE\n\n");
       }
    }
+   limparTela();
+   return jogador;
 }
 
 int definirDificuldade() {
    int dificuldade = 0;
    while(dificuldade < 1 || dificuldade > 2) {
-      printf("\n\t\t\tESCOLHA A DIFICULDADE: \n");
+      printf("\n\t\t  ESCOLHA A DIFICULDADE: \n");
       printf("\n\t---------------------------------------\n");
       printf("\n\t\t\t1 - facil \n \t\t\t2 - medio \n");
       printf("\n\t---------------------------------------\n");
-      printf("\n\t\t\tESCOLHA UMA OPCAO: ");
+      printf("\n\t\tESCOLHA UMA OPCAO: ");
       scanf("%d", &dificuldade);
       if(dificuldade < 1 || dificuldade > 2) {
          limparTela();
          printf("\n\t\tOPCAO INVALIDA. TENTE NOVAMENTE\n\n");
       }
    }
+   limparTela();
+   return dificuldade;
 }
 
 int desenharTabuleiro(char casas[9]) {
@@ -63,6 +72,29 @@ int desenharTabuleiro(char casas[9]) {
    printf("\t %c | %c | %c \n",casas[3],casas[4],casas[5]);
    printf("\t ---------- \n");
    printf("\t %c | %c | %c \n",casas[6],casas[7],casas[8]);
+}
+
+int jogo() {
+   switch(definirJogador()) { // CHAMA A FUNCAO PARA DEFINIR O JOGADOR
+      case 1: // JOGAR COM UM AMIGO
+         printf("\nESCOLHEU AMIGO\n");
+      break;
+
+
+      case 2: // JOGAR COM O COMPUTADOR
+         printf("\nESOCLHEU CPU\n");
+         switch(definirDificuldade()) {
+            case 1:  // DIFICULDADE FACIL
+               printf("\nESCOLHEU FACIL\n");
+            break;
+
+
+            case 2:  // DIFICULDADE MEDIA
+            
+            break;
+         }
+      break;
+   }
 }
 
 int pausar(){
